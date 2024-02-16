c4e3d212076f4a9443c85d89238304c7441b2937
myeongheon <axm0219@naver.com>
2024-02-16 18:10:12 +0900

    feat: 토큰 제공자 추가

---
+++ BE/src/test/java/com/econovation/rere/config/jwt/TokenProviderTest.java
@@ -0,0 +1,108 @@
+package com.econovation.rere.config.jwt;
+
+import com.econovation.rere.domain.entity.User;
+import com.econovation.rere.domain.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Map;
+
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    // 1. generateToken() 검증 테스트
+    @DisplayName("generateToken(): 유저 정보와 만료 기간을 전달해 토큰을 만들 수 있다.")
+    @Test
+    void generateToken() {
+        // given
+        User testUser = userRepository.save(User.builder()
+                .loginId("axm0219")
+                .pw("1q2w3e4r!")
+                .build());
+        // when
+        String token = tokenProvider.generateToken(testUser, Duration.ofDays(14));
+        // then
+        Integer userId = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Integer.class);
+
+        assertThat(userId).isEqualTo(testUser.getUserId());
+    }
+
+
+    // 2. validToken() 검증 테스트
+    @DisplayName("validToken(): 만료된 토큰인 때에 유효성 검증에 실패한다.")
+    @Test
+    void validToken_invalidToken() {
+        // given
+        String token = JwtFactory.builder()
+                .expiration(new Date(new Date().getTime() - Duration.ofDays(7).toMillis()))
+                .build()
+                .createToken(jwtProperties);
+        // when
+        boolean result = tokenProvider.validToken(token);
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("validToken(): 유효한 토큰인 때에 유효성 검증에 성공한다.")
+    @Test
+    void validToken_validToken() {
+        // given
+        String token = JwtFactory.withDefaultValues().createToken(jwtProperties);
+        // when
+        boolean result = tokenProvider.validToken(token);
+        // then
+        assertThat(result).isTrue();
+    }
+
+
+    // 3. getAuthentication() 검증 테스트
+    @DisplayName("getAuthentication(): 토큰 기반으로 인증 정보를 가져올 수 있다.")
+    @Test
+    void getAuthentication() {
+        // given
+        String loginId = "axm0219";
+        String token = JwtFactory.builder()
+                .subject(loginId)
+                .build()
+                .createToken(jwtProperties);
+        // when
+        Authentication authentication = tokenProvider.getAuthentication(token);
+        // then
+        assertThat(((UserDetails) authentication.getPrincipal()).getUsername()).isEqualTo(loginId);
+    }
+
+
+    // 4. getUserId() 검증 테스트
+    @DisplayName("getUserId(): 토큰으로 유저 ID를 가져올 수 있다.")
+    @Test
+    void getUserId() {
+        // given
+        Integer userId = 1;
+        String token = JwtFactory.builder()
+                .claims(Map.of("id", userId))
+                .build()
+                .createToken(jwtProperties);
+        // when
+        Integer userIdByToken = tokenProvider.getUserId(token);
+        // then
+        assertThat(userIdByToken).isEqualTo(userId);
+    }
+}
